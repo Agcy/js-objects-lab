301143b1620b6d4ac0c4f82c68af7b76273c8b44
765407782@qq.com <765407782@qq.com>
2023-09-12 11:33:26 +0100

    Loop constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -10,6 +10,7 @@ const aCar = {
         model : 'Corolla',
         cc : 1.8
     },
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
     registration: { year: 20, stage: 1, county: "WD", number: 1058 },
     mileage : '10000',
     colour: {
@@ -21,8 +22,10 @@ const aCar = {
     }
   };
   console.log(aCar.owner);
+  
   // owner information
   console.log(aCar.owner + ' drives a ' + aCar.type.make);
+  
   // registration
   console.log(
     "Reg. = " +
@@ -32,11 +35,29 @@ const aCar = {
       "-" +
       aCar.registration.number
   );
+  
   // car information
   console.log('It is a '+aCar.colour.exterior+' car, '+aCar.mileage+' mileage, with '+aCar.colour.interior.texture+' interior')
+  
+  // previous owners information
   console.log(
     "First owner: " +
       aCar.previous_owners[0].name +
       ' - ' +
       aCar.previous_owners[0].address
   )
+
+  // previous owners information loop format
+  for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i].name);
+  }
+  
+  // loop display 1
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+  }
+
+  // loop display 2
+  for (let p in aCar.type)  {
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+  }
