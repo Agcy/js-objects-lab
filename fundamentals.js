6b3dd979a63655fa4b483e9472b3b5f6c10d1d2c
765407782@qq.com <765407782@qq.com>
2023-09-12 11:22:27 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,11 +1,16 @@
 const aCar = {
     owner : "Joe Bloggs",
-    type : 'Toyota Corolla 1.8',
-    year : '20',
-    stage: '1',
-    county: 'WD',
-    number: '1058',
-    registration : '201WD1058',
+    address : '3 Walkers Lane',
+    previous_owners: [
+        { name: "Pat Smith", address: "1 Main Street" },
+        { name: "Sheila Dwyer", address: "2 High Street" }
+    ],
+    type : {
+        make : 'Toyota',
+        model : 'Corolla',
+        cc : 1.8
+    },
+    registration: { year: 20, stage: 1, county: "WD", number: 1058 },
     mileage : '10000',
     colour: {
         exterior: 'red',
@@ -15,7 +20,23 @@ const aCar = {
         }
     }
   };
-  var Reg = aCar.year + aCar.stage + '-' + aCar.county + '-' + aCar.number;
   console.log(aCar.owner);
-  console.log(aCar.owner + ' drives a ' + aCar.type.make + '\n' + 'The registration is ' + Reg);
+  // owner information
+  console.log(aCar.owner + ' drives a ' + aCar.type.make);
+  // registration
+  console.log(
+    "Reg. = " +
+      aCar.registration.year +
+      "-" +
+      aCar.registration.county +
+      "-" +
+      aCar.registration.number
+  );
+  // car information
   console.log('It is a '+aCar.colour.exterior+' car, '+aCar.mileage+' mileage, with '+aCar.colour.interior.texture+' interior')
+  console.log(
+    "First owner: " +
+      aCar.previous_owners[0].name +
+      ' - ' +
+      aCar.previous_owners[0].address
+  )
