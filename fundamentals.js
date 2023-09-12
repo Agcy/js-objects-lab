226f5fef1bc08095babc75f466390db45b12f3b9
765407782@qq.com <765407782@qq.com>
2023-09-12 10:59:43 +0100

    Nested objects.

--- fundamentals.js
+++ fundamentals.js
@@ -1,7 +1,12 @@
 const aCar = {
     owner : "Joe Bloggs",
     type : 'Toyota Corolla 1.8',
+    year : '20',
+    stage: '1',
+    county: 'WD',
+    number: '1058',
     registration : '201WD1058'
   };
-  
+  var Reg = aCar.year + aCar.stage + '-' + aCar.county + '-' + aCar.number;
   console.log(aCar.owner);
+  console.log(aCar.owner + ' drives a ' + aCar.type.make + '\n' + 'The registration is ' + Reg);
