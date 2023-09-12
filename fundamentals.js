9e6e5c8a8532d74a68fe6b8d0b4f6394f0568228
765407782@qq.com <765407782@qq.com>
2023-09-12 11:09:48 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -5,8 +5,17 @@ const aCar = {
     stage: '1',
     county: 'WD',
     number: '1058',
-    registration : '201WD1058'
+    registration : '201WD1058',
+    mileage : '10000',
+    colour: {
+        exterior: 'red',
+        interior: {
+            texture: 'leather',
+            shade: 'cream'
+        }
+    }
   };
   var Reg = aCar.year + aCar.stage + '-' + aCar.county + '-' + aCar.number;
   console.log(aCar.owner);
   console.log(aCar.owner + ' drives a ' + aCar.type.make + '\n' + 'The registration is ' + Reg);
+  console.log('It is a '+aCar.colour.exterior+' car, '+aCar.mileage+' mileage, with '+aCar.colour.interior.texture+' interior')
